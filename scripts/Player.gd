435e1301c01f37030403dbc885277e72b859b409
Damian Thomas <dimitrisonic@gmail.com>
2024-01-19 14:38:37 -0500

    basic point light test

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,10 @@
 extends Node2D
-@export var speed = 8;
+@export var speed: int;
 var velocity = Vector2.ZERO;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass # Replace with funcdtion body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
