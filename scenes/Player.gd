2764d8cf42769a6a88b34d40a2067ad296717a97
Damian Thomas <dimitrisonic@gmail.com>
2024-01-19 11:50:08 -0500

    basic animation switching; sprite scale increased

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @export var speed = 8;
+var velocity = Vector2.ZERO;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,16 +9,36 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var move_pressed = false;
+	# player movement
 	if Input.is_action_pressed("move_up"):
-		position.y -= speed;
+		velocity.y -= speed;
 		move_pressed = true;
 	if Input.is_action_pressed("move_down"):
-		position.y += speed;
+		velocity.y += speed;
 		move_pressed = true;
 	if Input.is_action_pressed("move_left"):
-		position.x -= speed;
+		velocity.x -= speed;
 		move_pressed = true;
 	if Input.is_action_pressed("move_right"):
-		position.x += speed;
+		velocity.x += speed;
 		move_pressed = true;
 
+	position += velocity;
+
+
+	# animation handling
+	if velocity.x > 0:
+		$Sprite.animation = "move_right";
+	if velocity.x < 0:
+		$Sprite.animation = "move_left";
+	if velocity.y > 0:
+		$Sprite.animation = "move_down";
+	if velocity.y < 0:
+		$Sprite.animation = "move_up";
+
+	if velocity.length() == 0:
+		$Sprite.pause();
+	else:
+		$Sprite.play();
+
+	velocity = Vector2.ZERO;
